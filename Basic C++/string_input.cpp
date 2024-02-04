f06f0db128ea0851198daaf6ca6619f067c1db14
Ittehad Abdullah <ittehadabdullah2000@gmai.com>
2024-02-04 07:39:19 +0600

    created dynamic integer variable

---
+++ Basic C++/string_input.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    char s[100];
+    cin.getline(s, 100);
+    // fgets(s, 100, stdin); gives output with enter 
+    cout << s << " "<< strlen(s);
+    return 0;
+}
---
+++ Dynamic Memory Allocation/dynamic_data.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // Heap memory te directly memory use kora jay na. Memory theke ekta refernece create hoy variable ta identify korte. Tai pointer diye memory allocate kore rakhte hoy. Example: int* a = new int. new int allocates memory reference in heap mmeory, int*a used to save the memory address from heap
+    int *a = new int;
+    *a = 10;
+
+    // will return the address of heap
+    cout << a << endl; 
+
+    // so we need to sdo dereferencing
+    cout << *a << endl;
+    return 0;
+}
